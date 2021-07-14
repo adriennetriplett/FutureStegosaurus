9d5047341d806976283c136334234c7d9d8b0bfc
adriennetriplett <adrienne.patterson.24@gmail.com>
2021-07-14 16:19:58 -0400

    added js app to check if open

---
+++ checkHours.js
@@ -0,0 +1,8 @@
+function checkHours() {
+	var hour = new Date().getHours();
+	if(hour >= 16 || hour <= 2) {
+		alert("Future Stegosaurus is currently open");
+	} else {
+		alert("Future Stegosaurus is currently closed");
+	}
+}
