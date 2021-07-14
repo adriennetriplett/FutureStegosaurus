3e74935744bd2c1612a1b183ca95d9ef4cbc4b37
adriennetriplett <adrienne.patterson.24@gmail.com>
2021-07-14 16:48:15 -0400

    added punctuation

--- checkHours.js
+++ checkHours.js
@@ -1,8 +1,8 @@
 function checkHours() {
 	var hour = new Date().getHours();
 	if(hour >= 16 || hour <= 2) {
-		alert("Future Stegosaurus is currently open");
+		alert("Future Stegosaurus is currently open!");
 	} else {
-		alert("Future Stegosaurus is currently closed");
+		alert("Future Stegosaurus is currently closed.");
 	}
 }
